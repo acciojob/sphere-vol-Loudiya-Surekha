f3890994ee3492f2231f8581d0487ed6de4e2200
Loudiya Surekha <lsurekha2002@gmail.com>
2025-06-21 09:59:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function volume_sphere() {
-    //Write your code here
-  
+    //Write your code 
+	let rad=document.getElementById("radius").value;
+	let radius=parseFloat(rad)
+	let vol=document.getElementById("volume");
+
+	if(isNaN(radius) || radius<0){
+		vol.value="NaN";
+	}else{
+		let volume=(4/3)*Math.PI*Math.pow(radius, 3);
+		vol.value=volume.toFixed(4);
+	}
+  return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
